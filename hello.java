38ac4a3cecf85be4cd5b1e1e8e8bf4a0bb76fb47
Nandhini-kancha <149858546+Nandhini-kancha@users.noreply.github.com>
2024-11-09 16:28:03 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ class java{
     public static void main(String[] args)
     {
         System.out.println("Hello git ,welcome to explore more");
+        System.out.println("hello suprithika idiot");
     }
 }
