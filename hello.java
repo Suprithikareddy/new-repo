aa12baf7b70c902cb896c2f572045b3a20433b1e
Suprithikareddy <suprithikap@gmail.com>
2024-11-09 16:33:53 +0530

    commiting

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-import java.util.*;til.*;
+import java.util.*;
 
 class java{
     public static void main(String[] args)
     {
-        System.out.println("Hello git ,welcome to explore more");
+        System.out.println("Hello git");
     }
 }
