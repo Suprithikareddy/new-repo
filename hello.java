c97a1925b0a3235e92bb6a5d7d3c9ed380b0ec13
VSaipuja <saipujavadla@gmail.com>
2024-11-09 02:39:41 -0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ import java.util.*;til.*;
 class java{
     public static void main(String[] args)
     {
-        System.out.println("Hello git");
+        System.out.println("Hello git ,welcome to explore more");
     }
 }
