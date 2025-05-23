d106d08fe34c81fbccf0ac8724cee3258c0284bf
Suprithikareddy <suprithikap@gmail.com>
2025-05-24 04:18:21 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,5 @@ class java{
     public static void main(String[] args)
     {
         System.out.println("Hello git ,welcome to explore more");
-        System.out.println("hello suprithika idiot");
-        System.out.println("love you bacheyy");
     }
 }
